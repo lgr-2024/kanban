1f12ac19e4239673520211e3c34d76ec7d734fc7
lgr-2024 <lgr@devtools.kr>
2024-02-07 19:20:07 +0900

    [FEAT] 공통 컴포넌트 분리

---
+++ src/components/CommentEditor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bg-white border-2 hover:border-tw-border-focused">
+    <div class="flex justify-between p-2 bg-green-300">
+      <div class="flex">
+        <!-- 확인 -->
+        <span class="flex">
+          <div class="flex transparent-style transparent-active transition-ease">
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-a"></i></span>
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-chevron-down"></i></span>
+          </div>
+          <span class="bg-tw-border w-[1px] h-6  leading-6 mx-2"></span>
+        </span>
+        <span class="flex">
+          <div class="flex">
+            <span class="text-center transparent-style transparent-active transition-ease"><i
+                class="w-6 h-6 leading-6 fa-solid fa-b"></i></span>
+            <span class="text-center transparent-style transparent-active transition-ease"><i
+                class="w-6 h-6 leading-6 fa-solid fa-italic"></i></span>
+            <span class="text-center transparent-style transparent-active transition-ease"><i
+                class="w-6 h-6 leading-6 fa-solid fa-ellipsis"></i></span>
+          </div>
+          <span class="bg-tw-border w-[1px] h-6  leading-6 mx-2"></span>
+        </span>
+        <span class="flex">
+          <div class="flex transparent-style transparent-active transition-ease">
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-list"></i></span>
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-chevron-down"></i></span>
+          </div>
+          <span class="bg-tw-border w-[1px] h-6  leading-6 mx-2"></span>
+        </span>
+        <span class="flex">
+          <div class="flex transparent-style transparent-active transition-ease">
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-plus"></i></span>
+            <span class="text-center"><i class="w-6 h-6 leading-6 fa-solid fa-chevron-down"></i></span>
+          </div>
+        </span>
+      </div>
+      <div class="flex gap-2">
+        <span class="transparent-style transparent-active transition-ease"><i
+            class="w-6 h-6 leading-6 fa-solid fa-paperclip"></i></span>
+        <span class="transparent-style transparent-active transition-ease"><i
+            class="w-6 h-6 leading-6 fa-solid fa-question"></i></span>
+      </div>
+    </div>
+    <div>
+      <div class="p-5 text-tw-text-subtlest">WRITE A COMMENT...</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/components/MainInPopup.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex-1 pb-2 pl-4 pr-2">
+    <div>
+      <h3 class="text-xs font-semibold text-tw-text-subtle">
+        <div class="absolute left-[-40px]">
+          <i class="fa-solid fa-table"></i>
+        </div>
+        Notifications
+      </h3>
+      <button @click='isActiveWatching' class="neutral-style neutral-active">{{ activeWatching }}</button>
+    </div>
+    <div>
+      <h3 class='relative py-2 mt-4 ml-10 text-base font-semibold text-tw-text-subtle'>
+        <div class="absolute left-[-40px]">
+          <i class="fa-solid fa-align-justify"></i>
+        </div>
+        Description
+      </h3>
+      <div class="ml-10 neutral-style neutral-active">
+        <a v-if="!activeDescriptionEditing" href="#" @click="isActiveDescriptionEditing" class="font-medium">Add a more
+          detailed
+          description...</a>
+      </div>
+    </div>
+    <div>
+      <h3 class='flex items-center justify-between py-2 mt-4 ml-10 text-base font-semibold text-tw-text-subtle'>
+        <span>Activity</span>
+        <button class="py-1.5 px-3 neutral-active" @click="isActiveHideDetails">{{ activeDetailsName }}</button>
+      </h3>
+      <div class="relative ml-10">
+        <div class="absolute left-[-40px]">
+          <div class="flex items-center justify-center w-8 h-8 rounded-full bg-tw-icon-accent-orange">
+            <i class="fa-solid fa-user"></i>
+          </div>
+        </div>
+        <div>
+          <input placeholder="Write a comment..."
+            class="mb-2 border-none text-tw-text-subtlest input-textarea-style input-textarea-active shadow-raised transition-ease" />
+        </div>
+        <!-- <CommentEditor /> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// import CommentEditor from './CommentEditor.vue'
+
+let activeWatching = ref('Watch')
+let activeDescriptionEditing = ref(false)
+let activeDetailsName = ref('Show details')
+let focusCommentEditor = ref(false)
+
+const isActiveWatching = () => {
+  if (activeWatching.value === 'Watch') {
+    activeWatching.value = 'Watching'
+  } else if (activeWatching.value === 'Watching') {
+    activeWatching.value = 'Watch'
+  }
+}
+const isActiveDescriptionEditing = () => {
+
+}
+const isActiveHideDetails = () => {
+  const defaultViewName = activeDetailsName.value === 'Show details'
+
+  if (defaultViewName) {
+    activeDetailsName.value = 'Hide details'
+  } else {
+    activeDetailsName.value = 'Show details'
+  }
+}
+const isFocusCommentEditor = () => {
+  focusCommentEditor.value = !focusCommentEditor.value
+}
+
+</script>
+
+<style lang="scss" scoped></style>
