e429c624ce8357d3c23522dc9e1c398d426d420b
lgr-2024 <lgr@devtools.kr>
2024-02-12 16:44:57 +0900

    [FEAT] DragAndDrop 기능 구현 (완료)

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,111 +1,131 @@
 <template>
-  <ol>
-    <li
-      @drop="onDrop($event, 1)"
-      @dragenter.prevent
-      @dragover.prevent
-      ref="kanbanList"
-      class="kanban-list">
-      <span
-        v-for="item in getList(1)"
-        :key="item.id"
-        draggable="true"
-        @dragstart="startDrag($event, item, item.id)"
-        @dragend="endDrag($event)"
-        class="kanban-item">
-        {{ item.title }}
-      </span>
-    </li>
-    <li
-      @drop="onDrop($event, 2)"
-      @dragenter.prevent
-      @dragover.prevent
-      ref="kanbanList"
-      class="kanban-list">
-      <span
-        v-for="item in getList(2)"
-        :key="item.id"
-        draggable="true"
-        @dragstart="startDrag($event, item, item.id)"
-        @dragend="endDrag($event)"
-        class="kanban-item">
-        {{ item.title }}
-      </span>
-    </li>
-  </ol>
+  <div>
+    <draggable :list="kanbanLists" tag="ol" :disabled="!enabled" item-key="id" ghost-class="ghost" class="kanban-lists">
+      <template v-for="element in kanbanLists" :itemKey="element.id" #item="{ element }">
+        <div class="kanban-list">
+          <h3>{{ element.title }}</h3>
+          <template v-if="element.card.length > 0">
+            <draggable :list="element.card" tag="ul" group="list" ghost-class="ghost" class="list-group kanban-cards"
+              @drop="addCard(element)">
+              <template v-for="(element, index) in element.card" :itemKey="index" #item="{ element }"
+                class="list-group-item">
+                <div>{{ element.cardName }}</div>
+              </template>
+            </draggable>
+          </template>
+          <template v-else>
+            <draggable :list="element.card" tag="ul" group="list" class="list-group kanban-cards">
+              <template v-for="element in element.empty" #item="{ element }">
+                <div class="list-group-item empty-card">
+                  {{ element.cardName }}
+                </div>
+              </template>
+            </draggable>
+          </template>
+        </div>
+      </template>
+    </draggable>
+  </div>
+
+  <button @click="addListInKanbanLists">add List</button>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
+import draggable from 'vuedraggable'
 
-let items = ref([
-  { id: 0, title: "Item A", list: 1 },
-  { id: 1, title: "Item B", list: 1 },
-  { id: 2, title: "Item C", list: 2 },
+let enabled = ref(true)
+let kanbanLists = ref([
+  {
+    id: 0, title: "list A", card: [
+      { cardName: 'cardName A-1' },
+      { cardName: 'cardName A-2' },
+      { cardName: 'cardName A-3' },
+    ], empty: [
+      { emptyName: 'empty' }
+    ]
+  },
+  {
+    id: 1, title: "list B", card: [
+      { cardName: 'cardName B-1' },
+      { cardName: 'cardName B-2' },
+      { cardName: 'cardName B-3' },
+    ], empty: [
+      { emptyName: 'empty' }
+    ]
+  },
+  {
+    id: 2, title: "list C", card: [
+      { cardName: 'cardName C-1' },
+      { cardName: 'cardName C-2' },
+      { cardName: 'cardName C-3' },
+    ], empty: [
+      { emptyName: 'empty' }
+    ]
+  },
+  {
+    id: 3, title: "list D", card: [
+      { cardName: 'cardName D-1' },
+      { cardName: 'cardName D-2' },
+      { cardName: 'cardName D-3' },
+    ], empty: [
+      { emptyName: 'empty' }
+    ]
+  },
 ]);
 
-let activeDragging = ref(false);
-let kanbanItemsArray = [];
+const addListInKanbanLists = () => {
+  const newList = {
+    id: kanbanLists.value.length,
+    title: 'hello',
+    card: [
 
-onMounted(() => {
-  const kanbanItems = document.querySelectorAll(".kanban-list");
-  kanbanItemsArray = Array.from(kanbanItems);
-});
+    ],
+    empty: [
+      { emptyName: 'empty' }
+    ]
+  }
 
-const getList = list => {
-  return items.value.filter(item => item.list === list);
-};
+  kanbanLists.value.push(newList)
+}
 
-const startDrag = (event, item, index) => {
-  // console.log(activeDragging.value);
+const addCardInKanbanCards = (list) => {
+  list.card.push({ cardName: 'New Card' });
+}
 
-  setTimeout(() => event.target.classList.add("dragging"), 0);
-  event.dataTransfer.dropEffect = "move";
-  event.dataTransfer.effectAllowed = "move";
-  event.dataTransfer.setData("itemID", item.id);
-  event.dataTransfer.setData("oldIndex", index);
-
-  activeDragging.value = true;
-};
-
-const endDrag = event => {
-  event.preventDefault();
-  event.target.classList.remove("dragging");
-
-  activeDragging.value = false;
-};
-
-const onDrop = (event, list) => {
-  // const itemID = event.dataTransfer.getData("itemID");
-  // const item = items.value.find(item => item.id == itemID);
-  // item.list = list;
-
-  event.preventDefault();
-  const itemID = event.dataTransfer.getData("itemID");
-  const oldIndex = parseInt(event.dataTransfer.getData("oldIndex"));
-  const newIndex = Array.from(event.target.parentNode.children).indexOf(
-    event.target
-  );
-  const item = items.value.find(item => item.id == itemID);
-  item.list = list;
-
-  // 아이템의 위치를 변경
-  items.value.splice(oldIndex, 1);
-  items.value.splice(newIndex, 0, item);
-};
 </script>
 
 <style scoped>
-.kanban-list {
-  background-color: red;
-  height: 100px;
+.kanban-lists {
+  background-color: yellow;
   margin-bottom: 10px;
+
+  display: flex;
 }
-.kanban-item {
+
+.kanban-list {
+  display: inline-block;
+  background-color: green;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+
+.kanban-cards {
   display: inline-block;
   background-color: blue;
+  margin-bottom: 10px;
+  margin-right: 10px;
 }
-.dragging {
-  background-color: black;
+
+.kanban-card {
+  width: 100%;
+  background-color: blue;
+  margin-bottom: 10px;
+  margin-right: 10px;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: gray;
 }
 </style>
